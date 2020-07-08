21064352edaa0776ac376890ab78250ad19609df
Anish Kumar Mishra <46549397+Anish218@users.noreply.github.com>
2020-07-08 19:31:50 +0530

    Add files via upload

---
+++ Bear_and_Big_brother.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,count1=0;
+    cin>>a>>b;
+    while(a<=b)
+    {
+        a=3*a;
+        b=2*b;
+        count1++;
+    }
+    cout<<count1;
+
+    return 0;
+}
